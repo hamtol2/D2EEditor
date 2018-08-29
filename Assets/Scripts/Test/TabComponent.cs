597807ba6e181a82db2d88b82ab05bcf521bc2d1
Ronnie Jang <yenseb@hanmail.net>
2018-08-29 17:03:15 +0900

    탭 모드 작업중 코드 백업
    - 탭 모드 테스트 중
    - 탭 추가 / 제거 코드 테스트 거의 완료

---
+++ Assets/Scripts/Test/TabComponent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace REEL.Test
+{
+	public class TabComponent : MonoBehaviour
+	{
+        [SerializeField] private Sprite selectedSprite;
+        [SerializeField] private Sprite unselectedSprite;
+
+        [SerializeField] private int tabID = -1;
+        private Image tabImage;
+
+        [SerializeField] private TabManager tabManager;
+        [SerializeField] private bool isSelected = false;
+
+        private void Awake()
+        {
+            tabImage = GetComponent<Image>();
+            ChangeState(false);
+        }
+
+        public void CloseTab()
+        {
+            if (tabManager) tabManager.RemoveTab(this);
+        }
+
+        public void SetManager(TabManager tabManager)
+        {
+            this.tabManager = tabManager;
+        }
+
+        public void OnTabClicked()
+        {
+            if (tabManager) tabManager.ChangeTabState(tabID);
+        }
+
+        public void ChangeState(bool isSelected)
+        {
+            if (isSelected) tabImage.sprite = selectedSprite;
+            else tabImage.sprite = unselectedSprite;
+
+            this.isSelected = isSelected;
+        }
+
+        public Vector2 GetTabSize()
+        {
+            return tabImage.GetComponent<RectTransform>().sizeDelta;
+        }
+
+        public int GetTabID { get { return tabID; } }
+
+        public void SetTabID(int id)
+        {
+            tabID = id;
+        }
+
+        public bool GetSelectedState {  get { return isSelected; } }
+    }
+}
---
+++ Assets/Scripts/Test/TabManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace REEL.Test
+{
+	public class TabManager : MonoBehaviour
+	{
+        [SerializeField] public List<TabComponent> currentTabs = new List<TabComponent>();
+        [SerializeField] private GameObject tabPrefab;
+        [SerializeField] private float tabOffset = 15f;
+        [SerializeField] private RectTransform newTabComponent;
+
+        [SerializeField] private int selectedTabIndex = -1;
+        [SerializeField] private int maxTabCount = 8;
+
+        public void AddTab()
+        {
+            if (currentTabs.Count >= 8) return;
+
+            currentTabs.Add(CreateNewTab());
+            RearrangeTabsPosition();
+        }
+
+        public void RemoveTab(TabComponent tab)
+        {
+            TabComponent foundTab = GetTab(tab.GetTabID);
+            if (foundTab != null)
+            {
+                currentTabs.RemoveAt(foundTab.GetTabID);
+                Destroy(foundTab.gameObject);
+            }
+
+            RearrangeTabsPosition();
+        }
+
+        private void SetAllTabUnselected()
+        {
+            for (int ix = 0; ix < currentTabs.Count; ++ix)
+            {
+                currentTabs[ix].ChangeState(false);
+            }
+        }
+
+        public void ChangeTabState(int tabID)
+        {
+            SetAllTabUnselected();
+            selectedTabIndex = tabID;
+            currentTabs[selectedTabIndex].ChangeState(true);
+        }
+
+        private TabComponent GetTab(int tabID)
+        {
+            for (int ix = 0; ix < currentTabs.Count; ++ix)
+            {
+                if (currentTabs[ix].GetTabID.Equals(tabID)) return currentTabs[ix];
+            }
+
+            return null;
+        }
+
+        private TabComponent CreateNewTab()
+        {
+            GameObject newTabObj = Instantiate(tabPrefab) as GameObject;
+            newTabObj.transform.SetParent(transform);
+            newTabObj.transform.position = Vector3.zero;
+            newTabObj.transform.localScale = Vector3.one;
+
+            TabComponent newTab = newTabObj.GetComponent<TabComponent>();
+            newTab.SetManager(this);
+
+            return newTab;
+        }
+
+        private void RearrangeTabsPosition()
+        {
+            if (currentTabs.Count == 0)
+            {
+                newTabComponent.anchoredPosition = Vector2.zero;
+                return;
+            }
+
+            bool anyTabSelected = false;
+            Vector2 newPos = Vector2.zero;
+            for (int ix = 0; ix < currentTabs.Count; ++ix)
+            {   
+                newPos.x = ix * currentTabs[ix].GetTabSize().x + (ix == 0? 0 : ix) * tabOffset;
+                newPos.y = 0f;
+                currentTabs[ix].GetComponent<RectTransform>().anchoredPosition = newPos;
+                currentTabs[ix].SetTabID(ix);
+
+                if (currentTabs[ix].GetSelectedState)
+                {
+                    anyTabSelected = true;
+                    selectedTabIndex = ix;
+                }
+            }
+
+            newTabComponent.anchoredPosition = new Vector2(newPos.x + currentTabs[0].GetTabSize().x + tabOffset, 0f);
+
+            if (currentTabs.Count == 1 || !anyTabSelected) ChangeTabState(0);
+        }
+	}
+}
