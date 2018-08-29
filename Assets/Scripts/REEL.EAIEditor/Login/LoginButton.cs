6c6d3f905417924959bc3fb19278941133211bd1
Ronnie Jang <yenseb@hanmail.net>
2018-08-29 11:32:09 +0900

    Login 씬 추가
    - 로그인 씬 추가

---
+++ Assets/Scripts/REEL.EAIEditor/Login/LoginButton.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace REEL.EAIEditor
+{
+	public class LoginButton : MonoBehaviour
+	{
+        [SerializeField] private string sceneName = "D2EEditor_Editor";
+        [SerializeField] private string requestIP = "";
+        [SerializeField] private InputField idField;
+        [SerializeField] private InputField pwField;
+        [SerializeField] private Toggle keepLoginStatusToggle;
+
+		public void OnLoginButtonCliced()
+        {
+            Debug.Log(idField.text + " : " + pwField.text + " : " + keepLoginStatusToggle.isOn);
+
+            // Try Login.
+            //StartCoroutine(RequestLogin(idField.text, pwField.text));
+
+            // Move to work space scene.
+            SceneManager.LoadScene(sceneName);
+        }
+
+        private void MoveMainScene()
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        
+        // Request login with input.
+        IEnumerator RequestLogin(string id, string pw)
+        {
+            WWW www = new WWW(requestIP + "/" + id);
+            WWWForm form = new WWWForm();
+            form.AddField("password", pw);
+            yield return www;
+
+            // Check Result.
+            string resultString = www.text;
+            LoginResult result = JsonUtility.FromJson<LoginResult>(resultString);
+
+            // test print.
+            Debug.Log(result);
+        }
+
+        [System.Serializable]
+        public class LoginResult
+        {
+            public bool success;
+            public int code;
+            public string message;
+            public string id;
+
+            public override string ToString()
+            {
+                return success.ToString() + " : " + code.ToString() + " : " + message + " : " + id;
+            }
+        }
+	}
+}
